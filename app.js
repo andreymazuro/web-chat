c4495896399b4749d287903ae68a26f91f818b9b
Andrey Mazuro <andrey.mazuro@icloud.com>
2017-02-26 15:50:56 +0300

    final v

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ app.use(express.static('assets'))
 users = [];
 io.on('connection', function(socket){
 
-    socket.on('setUsername', function(data){
-    if(users.indexOf(data) > -1 || data == ''){
+  socket.on('setUsername', function(data){
+    if (users.indexOf(data) > -1 || data == '') {
       socket.emit('userExists', 'Take another username!');
     }
-    else{
+    else
+    {
       socket.nickname = data
       users.push(data);
       socket.emit('userSet', {username: data});
@@ -27,23 +28,24 @@ io.on('connection', function(socket){
   });
 
   socket.on('msg', function(data){
-      //Send message to everyone
-      io.sockets.emit('newmsg', data);
+    io.sockets.emit('newmsg', data);
   })
 
   socket.on('disconnect', function() {
-      users.map(function(item,index){
+    users.map(function(item,index){
       if (item == socket.nickname) {
-          users.splice(index,1)
+        users.splice(index,1)
       }
     })
+
     io.sockets.emit('usersList', {users:users})
     if (socket.nickname != undefined) {
-    socket.broadcast.emit('newmsg', {message: socket.nickname + ' left chat', user: '' });
-  }
+      socket.broadcast.emit('newmsg', {message: socket.nickname + ' left chat', user: '' });
+    }
   });
 
 });
+
 var port = process.env.PORT || 3000;
 http.listen(port, function(){
   console.log('listening on localhost:3000');
